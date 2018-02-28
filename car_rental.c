de8e9b1cd927c0ffc4d6efb6dd2950c174f5506d
hasnank <hasnurk@gmail.com>
2018-02-28 17:24:06 +0700

    yeay

--- car_rental.c
+++ car_rental.c
@@ -26,14 +26,149 @@
 
 int waiting_time, unload_a, unload_b, load_a, load_b, speed,length_simulation, interarrival_rate_1, interarrival_rate_2, interarrival_rate_3, num_bus, num_location, i, j, num_seats,
   num_person[MAX_NUM_LOCATION+ 1],
-  route[MAX_NUM_LOCATION + 1][MAX_NUM_BUS + 1], num_seats_taken[MAX_NUM_BUS + 1], location, person;
+  route[MAX_NUM_LOCATION + 1][MAX_NUM_BUS + 1], num_seats_taken, destination, person, bus_position;
 double mean_interarrival_1, mean_interarrival_2, mean_interarrival_3, prob_distrib_destination[26],
-  mean_service[MAX_NUM_LOCATION + 1][MAX_NUM_BUS + 1], destination[MAX_NUM_LOCATION+1];
+  mean_service[MAX_NUM_LOCATION + 1][MAX_NUM_BUS + 1], distance[MAX_NUM_LOCATION+1][MAX_NUM_LOCATION+1], load_time, unload_time;
 FILE *infile, *outfile;
+
+void
+arrive (int location_number)		/* Function to serve as both an arrival event of a job
+				   to the system, as well as the non-event of a job's
+				   arriving to a subsequent station along its
+				   route. */
+{
+  
+  /* If this is a new arrival to the system, generate the time of the next
+     arrival and determine the job type and task number of the arriving
+     job. */
+
+  if (location_number == 1)
+    {
+
+      event_schedule (sim_time + expon (mean_interarrival_1, STREAM_INTERARRIVAL_1), EVENT_ARRIVAL_1);
+      destination = 3;
+      transfer[1] = sim_time;
+      transfer[2] = destination;
+      //transfer[3] = person;
+      list_file (LAST, 1);
+    }
+    
+  else if (location_number == 2)
+    {
+
+      event_schedule (sim_time + expon (mean_interarrival_2, STREAM_INTERARRIVAL_2), EVENT_ARRIVAL_2);
+      destination = 3;
+      transfer[1] = sim_time;
+      transfer[2] = destination;
+      //transfer[3] = person;
+      list_file (LAST, 2);
+    }
+    
+  else if (location_number == 3)
+    {
+
+      event_schedule (sim_time + expon (mean_interarrival_3, STREAM_INTERARRIVAL_3), EVENT_ARRIVAL_3);
+      double prob_dest = lcgrand (STREAM_DESTINATION);
+      if (prob_dest <= prob_distrib_destination[1])
+		{
+		  destination = 1;
+	    }
+	  else
+	    {
+		  destination = 2;
+		}
+      transfer[1] = sim_time;
+      transfer[2] = destination;
+      //transfer[3] = person;
+      list_file (LAST, 3);
+    }
+
+  /* Determine the station from the route matrix. */
+
+  //station = route[job_type][task];
+
+  /* Check to see whether bus is in my location. */
+
+  if (bus_position == location_number)
+    {
+	  load ();	  
+	}
+}
+
+void
+load (void)
+{
+  
+  /* Check to see if all seats are taken. */
+  if (num_seats_taken == MAX_SEATS)
+	{
+
+	  /* All seats in this bus are taken, so place the arriving job at
+		 the end of the appropriate queue. Note that the following data are
+		 stored in the record for each job:
+		 1. Time of arrival to this station.
+		 2. Job type.
+		 3. Current task number. */
+
+	  //transfer[1] = sim_time;
+	  //transfer[2] = job_type;
+	  //transfer[3] = task;
+	  //list_file (LAST, station);
+	}
+
+  else
+	{
+	  
+	  /* A seat in this bus is available, so passenger can come in. */
+	  list_remove (FIRST, bus_position);
+	  //sampst (0.0, station);	/* For station. */
+	  sampst (sim_time - transfer[1], bus_position);	/* For job type. */
+	  ++num_seats_taken;
+	  
+	  transfer[1] = sim_time;
+	  list_file(LAST, 4);
+	  
+	  timest ((double) num_seats_taken, bus_position);
+
+	  /* Schedule a service completion.  Note defining attributes beyond the
+		 first two for the event record before invoking event_schedule. */
+
+	  transfer[3] = destination;
+	  //transfer[4] = task;
+	  event_schedule (sim_time + distance[bus_position][transfer[3]] + load_time, EVENT_DEPARTURE);
+	  
+	  load_time += uniform(load_a, load_b, STREAM_LOADING);
+	}
+}
+
+void
+depart (void)
+{
+  
+	  /* Schedule a service completion.  Note defining attributes beyond the
+		 first two for the event record before invoking event_schedule. */
+	  
+	  unload_time += uniform(unload_a, unload_b, STREAM_UNLOADING);
+	  --num_seats_taken;
+}
+
+void
+move (void)
+{
+  if (bus_position == 3)
+  {
+	bus_position = 1;
+  } else bus_position++;
+  load_time = 0;
+  unload_time = 0;
+}
 
 int
 main ()				/* Main function. */
 {
+  bus_position = 3;
+  load_time = 0;
+  unload_time = 0;
   /* Open input and output files. */
 
   infile = fopen ("car_rental.in", "r");
@@ -62,11 +197,16 @@ main ()				/* Main function. */
 
   // JARAK COUNTER CLOCKWISE LOCATION 3->1, 1->2, 2->3 in MILES
   for (i = 1; i <= num_location; ++i)
-    fscanf (infile, "%lg", &destination[i]);
+  {
+	for (j = 1; j <=num_location; ++j)
+	{
+	  fscanf (infile, "%lg", &distance[i][j]);
+	}
+  }  
     
   // replace jarak dengan waktu tempuh  
   for (i = 1; i<= num_location; ++i) {
-	destination[i] = speed/destination[i];
+	distance[i] = speed/distance[i];
   }
 
   // UNLOAD SECONDS
@@ -132,6 +272,9 @@ main ()				/* Main function. */
 	  break;
 	case EVENT_DEPARTURE:
 	  depart ();
+	  break;
+	case EVENT_MOVE:
+	  move ();
 	  break;
 	case EVENT_END_SIMULATION:
 	  report ();
